841542a7e42a402078e968dcda344a9f96049cb1
HarisZahoor001 <hariszahoor1022@gmail.com>
2025-12-02 20:24:08 +0500

    UserProfile Model & Serializer Implemented

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from api.views import *
 
 urlpatterns = [
-    path('users/',view=UserProfileListCreate.as_view(),name="user-view-create"),
+    path('users/',view=CreateUserProfile.as_view(),name="user-view-create"),
 ]
