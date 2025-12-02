80f182aa6c886afd36778be549016e78c682473c
HarisZahoor001 <hariszahoor1022@gmail.com>
2025-12-02 20:23:10 +0500

    UserProfile Model & Serializer Implemented

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import UserProfile
 from .serializers import UserProfileSerializer
+from rest_framework.permissions import AllowAny,IsAuthenticated
 
 # Create your views here.
 
-class UserProfileListCreate(generics.ListCreateAPIView):
+class CreateUserProfile(generics.CreateAPIView):
     queryset=UserProfile.objects.all()
     serializer_class =UserProfileSerializer
+    permission_classes=[AllowAny]
