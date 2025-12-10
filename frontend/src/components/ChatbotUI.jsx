869e45d79b3ef7eb351b01d6b25adc8c50871046
HarisZahoor001 <hariszahoor1022@gmail.com>
2025-12-10 14:19:31 +0500

    Chat UI Improved

--- frontend/src/components/ChatbotUI.jsx
+++ frontend/src/components/ChatbotUI.jsx
@@ -8,9 +8,7 @@ import { FaArrowUp, FaUser, FaRobot } from "react-icons/fa";
 import api from '../api';
 
 export default function ChatbotUI() {
-    const OPENAI_API_KEY = "sks-proj-qOs7k67pRkQs7sRBYVM1RskQ-Nok8rGVVXwrs8dehrBC8-Ztps6_2xnIn8WV1uGHhRySWJdQ43T3BlbkFJv5p-LOD-GT1uy8JwckNPxPdTP6rftbLsy6D-D7goWh4-KB4-NuhWD9Ubpyk3dLqHgS-EN6txYA";
-
-    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const OPENAI_API_KEY = ''
     const [chatSliderOpen, setChatSliderOpen] = useState(false); 
     const hot_careers = ["AI Engineering", "Data Mining", "Data Science"];
     const [topic, setTopic] = useState("");
