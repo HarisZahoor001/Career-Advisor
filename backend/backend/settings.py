0eecf5344ee48303c93762d6e4b9ec4c6637b2d8
HarisZahoor001 <hariszahoor1022@gmail.com>
2025-12-11 14:50:46 +0500

    requirement.txt added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^qnm$#a$b0-&3yx1=^!tr%go%_4(q+b^-a%hf97aszokh!hntp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
